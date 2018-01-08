70d755220f014f7ff10449d7a48dda9b4fa9bad2
Alex Booker <booker@booker.codes>
2018-01-08 16:58:11 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', expressHandlebars())
+app.set('view engine', 'handlebars')
+
+app.get('/', function(req, res) {
+  res.render('home')
+})
+
+const PORT = 3000
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log(`Running on port ${PORT}`)
+  }
+})
