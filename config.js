81d71bbf97e1c4c8aa92a6ff929d4664a8b7a34c
agrawal.aayushi <agrawal.aayushi13@.com>
2018-11-06 18:27:31 +0530

    Readme updated

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 const instanceLocator = "v1:us1:152e35ba-cb48-47b3-97c6-70f534ddd337"
 const testToken = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/152e35ba-cb48-47b3-97c6-70f534ddd337/token"
 const username = "aayushiagrawal"
-const roomId = 19372751
 
-export { instanceLocator, testToken, username, roomId }
+export { instanceLocator, testToken, username }
